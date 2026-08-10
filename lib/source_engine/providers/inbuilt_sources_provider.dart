4858be1e0883db1c5fe62f1539d21cb14950e4eb
Roshan <roshan.codespace@gmail.com>
2026-08-10 14:51:12 +0530

    Revert "Roshancodespace (#83)"
    This reverts commit ad4772a101b8f283fd2373b3dcf49dbe8c026afb.

--- lib/source_engine/providers/inbuilt_sources_provider.dart
+++ lib/source_engine/providers/inbuilt_sources_provider.dart
@@ -1,33 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:shonenx/core/network/http_client.dart';
-import 'package:shonenx/shared/providers/storage_provider.dart';
-import 'package:shonenx/core/remote_config/providers/remote_config_provider.dart';
-import 'package:shonenx/source_engine/inbuilt_sources/anime/anidb_source.dart';
-import 'package:shonenx/source_engine/inbuilt_sources/anime/gojo_source.dart';
-import 'package:shonenx/source_engine/inbuilt_sources/anime/anikoto_source.dart';
 import 'package:shonenx/source_engine/providers/anime_source.dart';
 import 'package:shonenx/source_engine/providers/manga_source.dart';
 
-final _inbuiltAnimeListProvider = Provider<List<AnimeSource>>((ref) {
-  final client = ref.watch(httpClientProvider);
-  final storage = ref.watch(sharedPreferencesProvider);
-  return [
-    GojoSource(client: client, storage: storage),
-    AnidbSource(client: client, storage: storage),
-    AnikotoSource(client: client, storage: storage),
-  ];
-});
-
 final inbuiltAnimeSourcesProvider = Provider<List<AnimeSource>>((ref) {
-  final inbuilt = ref.watch(_inbuiltAnimeListProvider);
-  final remoteConfig = ref.watch(remoteConfigServiceProvider);
-
-  return inbuilt
-      .where((s) => !remoteConfig.isSourceDisabled(s.sourceInfo.id))
-      .toList();
-});
+  return [];
+}, name: 'inbuiltAnimeSourcesProvider');
 
 final inbuiltMangaSourcesProvider = Provider<List<MangaSource>>((ref) {
-  return <MangaSource>[];
-});
+  return [];
+}, name: 'inbuiltMangaSourcesProvider');
